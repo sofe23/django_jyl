ca1bb01ff82e40fa66b42e443720190c9cb39c94
sofe23 <ji7912345@gmail.com>
2024-01-20 00:39:34 +0900

    3주차 업무 웹 서비스 기능 구현 완료

--- my_project/urls.py
+++ my_project/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('mysite/', include('mysite.urls')),
     path('', include('pages.urls')),
+    path('accounts/', include('accounts.urls'))
+    
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:content_id>/', views.detail, name='detail'),
+
 ]
